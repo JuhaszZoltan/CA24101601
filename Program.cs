d81b12e2053cda93e223e7cbb8596d56b94c6e7c
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-10-16 10:18:13 +0200

    asd

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string name = "Zoltan";
-
+Console.Write("write a number here: ");
+int number = int.Parse(Console.ReadLine());
+for (int i = 0; i < number; i++)
 Console.WriteLine($"Hello, {name}!");
 
 
